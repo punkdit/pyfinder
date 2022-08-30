5f3151165dff102233678d3da32e9f3a699fbc77
Simon Burton <simon@arrowtheory.com>
2022-08-30 13:28:23 +0100

    commit edits

---
+++ pyfinder/theories/make.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+time prover9 -f  cat.mace  cat_extra.mace sym_mon_cat.mace monoidal_add_cat.mace laplaza_cat.mace arrow_cat.mace
+# 3sec
+# inconsistent 
+
+
+
--- pyfinder/theories/run_mace4.sh
+++ pyfinder/theories/run_mace4.sh
@@ -5,3 +5,6 @@
 mace4 -f cat.mace sym_mon_cat.mace monoidal_add_cat.mace laplaza_cat.mace arrow_cat.mace > output.dump
 
 
+# takes 20 minutes, but it works:
+#prover9 -f cat.mace cat_extra.mace sym_mon_cat.mace sym_mon_goal.mace 
+
